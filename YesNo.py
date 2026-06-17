12325a8510c41756c04eb487a6761c282e559b8e
khadija <hinjabou@gmail.com>
2026-06-17 13:19:57 +0200

    update plus variance

--- YesNo.py
+++ YesNo.py
@@ -26,8 +26,8 @@ def viterbi(observations, A, B, pi , variance = 1):
     psi [0] = 0
 
     delta = [0] * len(A)  
-    delta[0] = pi[0] * gaussian(observations[0], B[0], variance)  # probability of the initial state
-    print("delta at time 0 : ", delta)
+    delta[0] = pi[0] * gaussian(observations[0], B[0], variance[0])  # probability of the initial state
+    #print("delta at time 0 : ", delta)
     
 
     # recursion
@@ -47,12 +47,12 @@ def viterbi(observations, A, B, pi , variance = 1):
 
             # max ( delta t-1(i) * aij ) * bi(ot)
             best_previous_score = max(best_previous)
-            new_delta[j] = best_previous_score * gaussian(observations[t], B[j], variance)
+            new_delta[j] = best_previous_score * gaussian(observations[t], B[j], variance[j])
             indix = best_previous.index(best_previous_score)
         
         psi[t] = indix
         delta = new_delta
-        print("delta at time ", t, " : ", delta)
+        #print("delta at time ", t, " : ", delta)
 
     psi[-1] = len(A) - 1  
     return delta[-1]*0.5, psi   # la probabilite de la fin 
@@ -82,7 +82,7 @@ def forward(observations, A, B, pi, variance = 1):
 
     alphas = list()   
     delta = [0] * len(A)  
-    delta[0] = pi[0] * gaussian(observations[0], B[0], variance)  # probability of the initial state
+    delta[0] = pi[0] * gaussian(observations[0], B[0], variance[0])  # probability of the initial state
     
     alphas.append(delta)
 
@@ -102,7 +102,7 @@ def forward(observations, A, B, pi, variance = 1):
 
             # sum ( delta t-1(i) * aij ) * bi(ot)
             best_previous_score = sum(best_previous)
-            new_delta[j] = best_previous_score * gaussian(observations[t], B[j], variance)
+            new_delta[j] = best_previous_score * gaussian(observations[t], B[j], variance[j])
         
         delta = new_delta
         alphas.append(delta)
@@ -127,7 +127,7 @@ def backward(observations, A, B, variance=1):
             best_previous = []
 
             for j in range(len(A)):
-                score = A[i][j] * gaussian(observations[t + 1], B[j]) * beta[j]
+                score = A[i][j] * gaussian(observations[t + 1], B[j], variance[j]) * beta[j]
                 best_previous.append(score)
 
             new_beta[i] = sum(best_previous)
@@ -149,8 +149,10 @@ def gamma(observations, A, B, pi, variance=1):
     for t in range(len(observations)):
         tmp = list()
         for i in range(len(A)):
-            tmp.append(alphas[t][i] * betas[t][i] / prob)
-
+            if prob == 0 :
+                tmp.append(0)
+            else :
+                tmp.append(alphas[t][i] * betas[t][i] / prob)
         gammas.append(tmp)
     
     return gammas 
@@ -168,8 +170,11 @@ def ksi(observations, A, B, pi, variance=1):
         for i in range(len(A)):
             lig = list()
             for j in range(len(A)):
-                bj = gaussian(observations[t+1], B[j])
-                lig.append(alphas[t][i] * A[i][j] * bj * betas[t + 1][j]/prob)
+                bj = gaussian(observations[t+1], B[j], variance[j])
+                if prob == 0 :
+                    lig.append(0)
+                else :
+                    lig.append(alphas[t][i] * A[i][j] * bj * betas[t + 1][j]/prob)
             matrix.append(lig)
         ksi.append(matrix)
 
@@ -178,10 +183,10 @@ def ksi(observations, A, B, pi, variance=1):
 
 def reestimate(observations, A, B, pi, variance = 1):
     """ this function will help us to reestimate the parameters of the HMM """
-    alphas = forward(observations, A, B, pi)
-    betas = backward(observations, A, B)
-    gammas = gamma(observations, A, B, pi)
-    xi = ksi(observations,A, B, pi)
+    alphas = forward(observations, A, B, pi, variance)
+    betas = backward(observations, A, B, variance)
+    gammas = gamma(observations, A, B, pi, variance)
+    xi = ksi(observations,A, B, pi, variance)
     
     newA = list()
     for i in range(len(A)):
@@ -193,7 +198,11 @@ def reestimate(observations, A, B, pi, variance = 1):
             numTrani = 0
             for t in range(len(observations) - 2):
                 numTrani += gammas[t][i]
-            row.append(numTranij / numTrani)
+            
+            if numTrani == 0 :
+                row.append(0)
+            else :
+                row.append(numTranij / numTrani)
         newA.append(row)
 
 
@@ -208,19 +217,38 @@ def reestimate(observations, A, B, pi, variance = 1):
         for t in range(len(observations)):
                 # a revoir B : observations[t] ou gaussian() 
                 expecNum += gammas[t][j] * observations[t]
-        newB.append(expecNum / expeStaj)
+        if expeStaj == 0 :
+            newB.append(0)
+        else :    
+            newB.append(expecNum / expeStaj)
 
-    return newA, newB
+    newVar = list()
+    for j in range(len(A)):
+        expeStaj = 0
+        for t in range(len(observations)):
+            expeStaj += gammas[t][j]
+        
+        nominator = 0
+        for t in range(len(observations)):
+            nominator += gammas[t][j] * (observations[t] - B[j]) * (observations[t] - B[j])**(len(observations))
+        
+        if expeStaj == 0 :
+            variance.append(0)
+        else : 
+            variance.append(nominator / expeStaj)
+
+
+    return newA, newB, variance
 
 
 def learning(observations, A, B, pi, variance):
     """ learning loop of the model """
-    oldA, oldB = A, B
-    for i in range(10):
-        newA, newB = reestimate(observations, oldA, oldB, pi)
-        oldA, oldB = newA, newB
+    oldA, oldB, oldVariance = A, B, variance
+    for i in range(15):
+        newA, newB, newVariance = reestimate(observations, oldA, oldB, pi, oldVariance)
+        oldA, oldB, oldVariance = newA, newB, newVariance
     
-    return oldA, oldB
+    return oldA, oldB, oldVariance
 
     
         
@@ -230,7 +258,7 @@ def learning(observations, A, B, pi, variance):
 
 # example 
 
-pi = [1, 0, 0]  
+pi = [1, 0, 0]
 
 # transition matrices
 YesA = [
@@ -252,18 +280,65 @@ YesStates = [2, 4, 3]
 
 NoStates = [3, 2, 1]
 
+  
+variance = [1, 1, 1]
+
+
 # observation sequence
 observation = [1, 3, 3]
 observation2 = [1, 2, 3, 3]
-observation3 = [1, 2, 3, 3, 1]
+
+
+
 
 # compute the probability of the observation sequence given the model
-print("\nP of the observation O given the Yes model: ", viterbi(observation, YesA, YesStates, pi))
+"""print("\nP of the observation O given the Yes model: ", viterbi(observation, YesA, YesStates, pi))
 print("\nP of the observation O given the No model: ", viterbi(observation, NoA, NoStates, pi))
 
-"""
+
 print("\nP of the observation O given the Yes model: ", viterbi(observation3, YesA, YesStates, pi)[0])
 print(f"the best hidden sequence for the Yes model is : {viterbi(observation3, YesA, YesStates, pi)[1]}")
 print("\nP of the observation O given the No model: ", viterbi(observation3, NoA, NoStates, pi)[0])
 print(f"the best hidden sequence for the No model is : {viterbi(observation3, NoA, NoStates, pi)[1]}")
+
 """
+
+ObYes = [1, 0.5, 1, 4, 4.5, 4, 3, 3.5, 3]
+ObNo = [4, 4, 3, 2, 2.5, 2, 1, 1.5, 1]
+
+print("\t\t the Yes model before learning ")
+print(YesA, YesStates)
+
+
+print("\nP of the observation O given the Yes model before learning : ", viterbi(ObYes, YesA, YesStates, pi, variance)[0])
+print(f"the best hidden sequence for the Yes model is : {viterbi(ObYes, YesA, YesStates, pi, variance)[1]}")
+
+
+print("\t\t the Yes model after learning ")
+newYesA, newYesStates, newYesVariance = learning(ObYes, YesA, YesStates, pi, variance)
+print(newYesA, newYesStates, newYesStates)
+
+print("\nP of the observation O given the Yes model after learning : ", viterbi(ObYes, newYesA, newYesStates, pi, newYesVariance)[0])
+print(f"the best hidden sequence for the Yes model is : {viterbi(ObYes, newYesA, newYesStates, pi, newYesVariance)[1]}")
+
+
+
+
+
+print("\t\t the No model before learning ")
+print(NoA, NoStates)
+
+
+print("\nP of the observation O given the No model before learning : ", viterbi(ObNo, YesA, YesStates, pi, variance)[0])
+print(f"the best hidden sequence for the No model is : {viterbi(ObNo, YesA, YesStates, pi, variance)[1]}")
+
+
+print("\t\t the No model after learning ")
+newYesA, newYesStates, newNoVariance = learning(ObNo, YesA, YesStates, pi, variance)
+print(newYesA, newYesStates, newNoVariance)
+
+print("\nP of the observation O given the No model after learning : ", viterbi(ObYes, newYesA, newYesStates, pi, newNoVariance)[0])
+print(f"the best hidden sequence for the No model is : {viterbi(ObYes, newYesA, newYesStates, pi, newNoVariance)[1]}")
+
+
+
